b4bc23cdd09f3f6b3f21a9b4e1d93063b16ec54a
prushton2 <peterrushton418@gmail.com>
2021-12-12 10:23:53 -0500

    Added a converter for url escape characters

--- encryption.js
+++ encryption.js
@@ -8,20 +8,55 @@ const createKeys = () => {
     return pair
 }
 
-module.exports.encrypt = (keyPair, message) => {
+module.exports.encrypt = (publicKey, message) => {
     let toEncrypt = Buffer.from(message, "utf8");
     let encrypted = crypto
-        .publicEncrypt(pair["public"], toEncrypt)
+        .publicEncrypt(publicKey, toEncrypt)
         .toString("base64");
     return encrypted
 }
 
-module.exports.decrypt = (keyPair, message) => {
+module.exports.decrypt = (privateKey, message) => {
     let toDecrypt = Buffer.from(message, "base64");
     let decrypted = crypto
-    .privateDecrypt(pair["private"], toDecrypt)
+    .privateDecrypt(privateKey, toDecrypt)
     .toString("utf8");
     return decrypted
+}
+
+module.exports.convertUrlEscapeCharacters = (string) => {
+    charmap = 
+    [[" ","%20"],
+    ["$", "%24"],
+    ["&", "%26"],
+    ["`", "%60"],
+    [":", "%3A"],
+    ["<", "%3C"],
+    [">", "%3E"],
+    ["[", "%5B"],
+    ["]", "%5D"],
+    ["{", "%7B"],
+    ["}", "%7D"],
+    ["“", "%22"],
+    ["+", "%2B"],
+    ["#", "%23"],
+    ["%", "%25"],
+    ["@", "%40"],
+    ["/", "%2F"],
+    [";", "%3B"],
+    ["=", "%3D"],
+    ["?", "%3F"],
+    ["\\","%5C"],
+    ["^", "%5E"],
+    ["|", "%7C"],
+    ["~", "%7E"],
+    ["‘", "%27"],
+    [",", "%2C"]]
+
+    charmap.forEach((element) => {
+        string = string.replace(element[1], element[0])
+    })
+    return string
 }
 
 function changeText() {
