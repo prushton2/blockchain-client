8ee76cc6788717edbabef5ae2a3b82d5b397d8f6
prushton2 <peterrushton418@gmail.com>
2021-12-12 09:36:43 -0500

    added functionality to access a users keypair

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const fs = require('fs');
 
 const cors = require("cors")
 const express = require('express');
+const res = require("express/lib/response");
 const app = express();
 
 const outPort=8080; 
 const inPort =5000;
 
-
+let activeUser;
 
 
 fs.readFile('./server/index.html', function (err, html) {
@@ -28,8 +29,22 @@ app.use(cors({
     origin: `http://localhost:${outPort}`
 }));
 
-app.get('/*', function (req, res) {
-    res.send('Hello World2');
+app.get('/', function (req, res) {
+    res.send('Hello World');
+})
+
+app.get("/setActiveUser/*", async(req, res) => {
+    keyPair = req.url.split("/")[2]
+    console.log(keyPair)
+    if(fs.existsSync(`keyPairs/${keyPair}/publicKey.pem`) && fs.existsSync(`keyPairs/${keyPair}/privateKey.pem`)) {
+        res.end(`Opened key pair: ${keyPair}`)
+        activeUser = keyPair
+    } 
+    else {
+        res.end("Invalid key pair")
+        activeUser = null
+    }
+
 })
 
 var server = app.listen(inPort, function () {
