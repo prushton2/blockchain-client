cb859c75d69fb33c7794c80202883f106e942a64
prushton2 <peterrushton418@gmail.com>
2021-12-16 10:06:11 -0500

    inherit

--- index.js
+++ index.js
@@ -34,30 +34,18 @@ app.use(cors({
 }));
 
 app.get("/NewUser/*", async(req, res) => {
-    console.log("received")
-
-
+    console.log(`Creating user`)
     keyPair = encryption.createKeys()
     
-    console.log("Created key pair")
-
     publicKey = keyPair["public"]
     publicKey = publicKey.replace(/\r?\n|\r/g, "-----NEWLINE-----")
 
-    console.log("Converted key pair to a URL acceptable string")
-
     userName = req.url.split("/")[2]
-
-    console.log(`Set desired username to ${userName}`)
-    console.log("Sending request to create user")
     
     getResponse = await requests.get(`${baseURL}/newUser/${userName}/${publicKey}`)
     
-    console.log(`Got a response of ${getResponse}`)
-
     if(getResponse == "Created User") {
         km.saveKeys(userName, keyPair["public"], keyPair["private"])
-        console.log("Saved key pair")
     }
     res.end(getResponse)
 })
@@ -67,15 +55,23 @@ app.get("/NewBlock/*", async(req, res) => {
     console.log("Creating new block: ",info)
 
     getResponse = await requests.get(`${baseURL}/newBlock/${activeUser}/${info}`)
-    getResponse = JSON.parse(getResponse)
-    otp = getResponse[0]
-    encryptedHash = getResponse[1]
+    try {
+        getResponse = JSON.parse(getResponse)
+        otp = getResponse[0]
+        encryptedHash = getResponse[1]
+    } catch (e) {
+        res.end("Invalid input parameters")
+    }
 
 
     privateKey = await km.getPrivateKey(activeUser)
-    decryptedHash = encryption.decrypt(privateKey, encryptedHash)
+    try {
+        decryptedHash = encryption.decrypt(privateKey, encryptedHash)
+        result = await requests.get(`${baseURL}/otp/${otp}/${decryptedHash}`)
+    } catch (e) {
+        res.end("Invalid Key Pair")
+    }
 
-    result = await requests.get(`${baseURL}/otp/${otp}/${decryptedHash}`)
     res.end("Created Block")
 })
 
@@ -91,37 +87,14 @@ app.get("/del/*", async(req, res) => {
     decryptedHash = encryption.decrypt(privateKey, encryptedHash)
 
     result = await requests.get(`${baseURL}/otp/${otp}/${decryptedHash}`)
-    console.log(result)
 
-    res.end(result)
+    res.end("Deleted")
 
 })
 
 app.get("/ls/*", async(req, res) => {
     message = req.url.split("/")[2]
     res.end(await requests.get(`${baseURL}/ls/${message}`))
-})
-
-app.get("/Encrypt/*", async(req, res) => {
-    message = req.url.split("/")[2]
-
-    f = await km.getPublicKey(activeUser)
-    encrypted = encryption.encrypt(f, message)
-    res.end(encrypted)
-})
-
-app.get("/Decrypt/*", async(req, res) => {
-    message = req.url.split("/").slice(2).join("/")
-    message = encryption.convertUrlEscapeCharacters(message)
-    
-    f = await km.getPrivateKey(activeUser)
-    try {
-        decrypted = encryption.decrypt(f, message)
-        decrypted = encryption.convertUrlEscapeCharacters(decrypted)
-        res.end(decrypted)
-    } catch (e) {
-        res.end("An Unexpected Error Occurred")
-    }
 })
 
 app.get("/setActiveUser/*", (req, res) => {
